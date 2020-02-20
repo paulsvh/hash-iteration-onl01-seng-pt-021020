46acfde8885f40b24ed97ec363de58fa4b9cf06c
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-20 00:43:19 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,7 +4,7 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     if age <= 12
       puts "Happy Birthday #{name}! You are now #{age} years old!"
